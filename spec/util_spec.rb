44da2ceaae97b5ab0e982aa7ef3a4bdd765d72ac
Tuomas Kareinen <tkareine@gmail.com>
2009-01-31 14:14:14 +0200

    Removed excess File.join calls.

--- spec/util_spec.rb
+++ spec/util_spec.rb
@@ -1,4 +1,4 @@
-require File.join(File.dirname(__FILE__) << "/searchable_record_spec_helper")
+require File.dirname(__FILE__) << "/searchable_record_spec_helper"
 require "active_support/core_ext/blank"
 require "util"
 
